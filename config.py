293283a3c75604ebe4b350d2b7557900f5694330
Altria Blika <blekakok@gmail.com>
2020-03-07 18:41:20 +0300

    initial commit

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ from decouple import config
 from playhouse.db_url import connect
 
 TOKEN = config("TOKEN")
-DB = connect(config("DB_URL", default="sqlite:///botttt.db"))
+DB = connect(config("DATABASE_URL", default="sqlite:///botttt.db"))
 PORT = config("PORT", default=8443, cast=int)
 HEROKU_APP_NAME = config("HEROKU_APP_NAME", default=None)
